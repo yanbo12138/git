b7fe79323fd0fab80ac0386f57c29839674b10d3
chan <chan@qq.com>
2021-09-30 22:23:40 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author cyb315
+ * @create 2021-09-30 22:19
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("你好傻逼！");
+    }
+}
