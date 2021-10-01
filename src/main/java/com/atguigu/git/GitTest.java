e0dc48dbc7bf9baff581821c4d055b07cc754e1d
chan <chan@qq.com>
2021-10-01 11:02:40 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好傻逼！");
         System.out.println("你好2逼！");
+        System.out.println("你好2逼！");
     }
 }
